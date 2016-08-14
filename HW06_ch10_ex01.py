4fe32cdc2688b6edf24f2f5a5bec6fae627551e0
Jolly <jollylst@hotmail.com>
2016-08-14 15:40:22 -0700

    Forgot to submit ch10_ex01

--- HW06_ch10_ex01.py
+++ HW06_ch10_ex01.py
@@ -5,3 +5,25 @@
 # In your final submission: 
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+def nested_sum(nested_list):
+	# turn the nested list into a new single list first
+	replacement = []
+	def new_list(nested_list):
+		for i in range(len(nested_list)):
+			if type(nested_list[i]) == int:
+				replacement.append(nested_list[i])
+			else:
+				new_list(nested_list[i])
+		return replacement
+	new_list(nested_list)
+	print(sum(replacement))
+
+
+lst = [1, 2, [3, [4, 5]]]
+nested_sum(lst)
+
+def main():
+	pass
+if __name__ == "__main__":
+	main()
